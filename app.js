c02ef452b6d038f6ba0d91350a4a59e62778bb32
cheikh diouf <cheikhdiouf096@gmail.com>
2024-05-29 11:13:04 +0000

    ramener le serveur  server.js

--- app.js
+++ app.js
@@ -171,87 +171,328 @@
 
 
 
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-app.use(express.json());
-const cors = require("cors");
-app.use(cors());
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// app.use(express.json());
+// const cors = require("cors");
+// app.use(cors());
 
 
-//mongodb connection
-const mongoUrl =
-  "mongodb+srv://adarsh:adarsh@cluster0.zllye.mongodb.net/?retryWrites=true&w=majority";
+// //mongodb connection
+// const mongoUrl =
+//   "mongodb+srv://adarsh:adarsh@cluster0.zllye.mongodb.net/?retryWrites=true&w=majority";
 
 
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((e) => console.log(e));
+// mongoose
+//   .connect(mongoUrl, {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to database");
+//   })
+//   .catch((e) => console.log(e));
 
 
-//importing schema
-require("./imageDetails");
-const Images = mongoose.model("ImageDetails");
+// //importing schema
+// require("./imageDetails");
+// const Images = mongoose.model("ImageDetails");
 
 
-app.get("/", async (req, res) => {
-  res.send("Success!!!!!!");
-});
+// app.get("/", async (req, res) => {
+//   res.send("Success!!!!!!");
+// });
 
 
-app.listen(5000, () => {
-  console.log("Server Started");
-});
+// app.listen(5000, () => {
+//   console.log("Server Started");
+// });
 
 
-//////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////
 
 
-const multer = require("multer");
+// const multer = require("multer");
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../src/images/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "../src/images/");
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now();
+//     cb(null, uniqueSuffix + file.originalname);
+//   },
+// });
 
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 
-app.post("/upload-image", upload.single("image"), async (req, res) => {
-  console.log(req.body);
-  const imageName = req.file.filename;
+// app.post("/upload-image", upload.single("image"), async (req, res) => {
+//   console.log(req.body);
+//   const imageName = req.file.filename;
 
 
-  try {
-    await Images.create({ image: imageName });
-    res.json({ status: "ok" });
-  } catch (error) {
-    res.json({ status: error });
-  }
-});
+//   try {
+//     await Images.create({ image: imageName });
+//     res.json({ status: "ok" });
+//   } catch (error) {
+//     res.json({ status: error });
+//   }
+// });
 
 
-app.get("/get-image", async (req, res) => {
-  try {
-    Images.find({}).then((data) => {
-      res.send({ status: "ok", data: data });
+// app.get("/get-image", async (req, res) => {
+//   try {
+//     Images.find({}).then((data) => {
+//       res.send({ status: "ok", data: data });
+//     });
+//   } catch (error) {
+//     res.json({ status: error });
+//   }
+// });
+
+
+
+
+
+// const Hotel = require('../models/hotel');
+
+// // exports.createHotel = async (req, res) => {
+// //   try {
+// //         const {nom, adress, email, telephone, devise, prix, imageUrl} = req.body;
+// //         const hotel = new Hotel({nom, adress, email, telephone, devise, prix, imageUrl})
+// //         await  hotel.save()
+// //         res.status(201).json(hotel)
+// //   } catch (error) {
+// //     res.status(400).json({message: error.message})
+// //   }
+// exports.createHotel = (req, res, next) => {
+//   const objetHotel = {
+//     nom: req.body.nom,
+//     adresse: req.body.adresse,
+//     email: req.body.email,
+//     numero: req.body.numero,
+//     prix: req.body.prix,
+//     devise: req.body.devise,
+//     image: req.body.image,
+//   };
+
+//   delete objetHotel._id;
+//   delete objetHotel._userId;
+
+//   const hotel = new Hotel({
+//     ...objetHotel,
+//     // userId: req.auth.userId,
+//     image: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+//   });
+
+//   hotel
+//     .save()
+//     .then(() => {
+//       res.status(201).json({
+//         message: "Hotel saved successfully!",
+//       });
+//     })
+//     .catch((error) => {
+//       res.status(400).json({
+//         error: error,
+//       });
+//     });
+// };
+
+//   // const hotel = new Hotel({
+//   //   nom: req.body.nom,
+//   //   adress: req.body.adress,
+//   //   email: req.body.email,
+//   //   telephone: req.body.telephone,
+//   //   devise: req.body.devise,
+//   //   prix: req.body.prix
+//   // // });
+//   // hotel.save().then(
+//   //   () => {
+//   //     res.status(201).json({
+//   //       message: 'Post saved successfully!'
+//   //     });
+//   //   }
+//   // ).catch(
+//   //   (error) => {
+//   //     res.status(400).json({
+//   //       error: error
+//   //     });
+//   //   }
+//   // );
+
+// // exports.getOneHotel = (req, res, next) => {
+// //   Hotel.findOne({
+// //     _id: req.params.id
+// //   }).then(
+// //     (hotel) => {
+// //       res.status(200).json(hotel);
+// //     }
+// //   ).catch(
+// //     (error) => {
+// //       res.status(404).json({
+// //         error: error
+// //       });
+// //     }
+// //   );
+// // };
+
+// // exports.modifyHotel = (req, res, next) => {
+// //   const hotel = new Hotel({
+// //     _id: req.params.id,
+// //     title: req.body.title,
+// //     description: req.body.description,
+// //     imageUrl: req.body.imageUrl,
+// //     price: req.body.price,
+// //     userId: req.body.userId
+// //   });
+// //   Hotel.updateOne({_id: req.params.id}, hotel).then(
+// //     () => {
+// //       res.status(201).json({
+// //         message: 'Thing updated successfully!'
+// //       });
+// //     }
+// //   ).catch(
+// //     (error) => {
+// //       res.status(400).json({
+// //         error: error
+// //       });
+// //     }
+// //   );
+// // };
+
+// // exports.deleteHotel = (req, res, next) => {
+// //   Hotel.deleteOne({_id: req.params.id}).then(
+// //     () => {
+// //       res.status(200).json({
+// //         message: 'Deleted!'
+// //       });
+// //     }
+// //   ).catch(
+// //     (error) => {
+// //       res.status(400).json({
+// //         error: error
+// //       });
+// //     }
+// //   );
+// // };
+
+// exports.getAllHotel = async (req, res) => {
+//   try{
+//     const hotels = await Hotel.find();
+//     res.json(hotels)
+//   }catch(error){
+//     res.status(500).json({message: error.message})
+//   }
+//   // Hotel.find().then(
+//   //   (hotel) => {
+//   //     res.status(200).json(hotel);
+//   //   }
+//   // ).catch(
+//   //   (error) => {
+//   //     res.status(400).json({
+//   //       error: error
+//   //     });
+//   //   }
+//   // );
+// };
+
+// in controllers/stuff.js
+
+const Thing = require("../models/thing");
+
+exports.createThing = (req, res, next) => {
+  const thing = {
+    nom: req.body.nom,
+    adresse: req.body.adresse,
+    prix: req.body.prix,
+    devise: req.body.devise,
+    telephone: req.body.telephone,
+    imageUrl: req.body.imageUrl,
+    email: req.body.email,
+  };
+  const hotel = new Thing({
+    ...thing,
+    // userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+  });
+
+  hotel
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Post saved successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
     });
-  } catch (error) {
-    res.json({ status: error });
-  }
-});
+};
 
+exports.getOneThing = (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id,
+  })
+    .then((thing) => {
+      res.status(200).json(thing);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
+};
 
+exports.modifyThing = (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    nom: req.body.nom,
+    adresse: req.body.adresse,
+    prix: req.body.prix,
+    devise: req.body.devise,
+    telephone: req.body.telephone,
+    image: req.body.image,
+  });
+  Thing.updateOne({ _id: req.params.id }, thing)
+    .then(() => {
+      res.status(201).json({
+        message: "Thing updated successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
 
+exports.deleteThing = (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Deleted!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+exports.getAllStuff = (req, res, next) => {
+  Thing.find()
+    .then((things) => {
+      res.status(200).json(things);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
